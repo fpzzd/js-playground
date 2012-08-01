a7f9ede77ba9c3abc45cade17f6da89a0a12a283
fpzzd <franklinpezzutidyer@gmail.com>
2012-07-31 22:30:18 -0400

    franklins page

---
+++ action2.js
@@ -0,0 +1,35 @@
+function whenUserClicks(){
+alert ("After three days in the scorching sun and freezing night, you would love to have somewhere for shelter. Unfortunately, after those three days, you find none.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

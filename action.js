e3894d57650a562c43735a80b00363c260ccbfcf
fpzzd <franklinpezzutidyer@gmail.com>
2012-08-23 22:10:29 -0300

    Update action.js

--- action.js
+++ action.js
@@ -1,5 +1,14 @@
-alert("Hello. My name is codesecret. My secret is secretpage. For more information, please log in. Oh, and where have you been?")
+function trim(str) {
+	trimmedStr = str.replace(/^\s+|\s+$/g,'');
+	return trimmedStr;
+}
+
+
 function whenUserClicks() {
+
+
+
+	console.log('hi');
 			
 	var USR = $('#UFI').val();
 	
@@ -7,58 +16,90 @@ function whenUserClicks() {
 	
 	var PV = $('#PV').val();
 	
-	var SQ = $("[name=securityquestion]:checked").val() 
+	var AG = $('#AG').val();
+	
+			
+	USR = trim (USR)
 
-	if (USR == "")
-	$("#message").text("You have entered no username. Please enter one.")
-	
-	else {
-	 
-	 if (PFI == "") {
-	 $("#message").text("Please enter a password.")
-	 } 
-	else {
-	
-	if (PV == ""){
-	$("#message").text("You have entered no password verification. Please enter one.")
-	}
-	
-	else {
-	
-	if (SQ == ""){
-	$("#message").text("You need to select a security question.")
-	}
-	
-	else {
-	
-	if (PFI == PV){
-	
-	if (SQ == "Where were you on the day of today?" && PFI == "secretpage" && USR == "codesecret")
-	alert("This is a secret page. Congratulations.")
-	
-		$("#message").text("You are successfully logged in, " + USR)
-	}
-	else {  
-	
-	
-	
-		var PFI = $('#PFI').val("");
-	
-		var PV = $('#PV').val("");
 
-		$("#message").text("Your password and password verification do not match, " + USR)
-	}}}}}	
+	
+	if (USR == "Franklin" && PFI == "dungwax3496" && PV == "dungwax3496" && AG == "still growing"){
+
+		alert ("welcome back, Franklin.")
+		
+		alert ("Oh, I nearly forgot to tell you. You got a message in your console. You had better check it.")
+		
+		console.log ("Refresh the page, your turn to use your account is over.")
+		
+		
+	}  
+	
+	else {
+	
+	
+		if (USR == "") {
+	
+			$("#message").text("You have entered no username. Please enter one.")
+		
+		} else if (PFI == "") {
+		
+			$("#message").text("Please enter a password.")
+	 	
+		} else if (PV == "") {
+			
+			$("#message").text("You have entered no password verification. Please enter one.")
+			
+		} else if (PV !== PFI){
+	
+
+			$("#message").text("Your password and password verification do not match, " + USR)
+	
+
+		} else if (AG == ""){
+		
+			$("#message").text("You have not entered your age, " + USR)
+
+		} else if (AG < 20) {
+		
+			$("#message").text("You are too young to create an account. Sorry.")
+		
+		} else {
+
+			$("#message").text("You are successfully logged in, " + USR)
+			
+			
+			
+		
+		}
+	
+	}
+	
+}  
+
+			
+		
+
+
+
 			
 	
 			
-/*	} else {
-		responseStr = 'Thank you for buying '+ $('#first-name').val() +', '+ $('#last-name').val()+'pies.'
-		$('#response').text(responseStr);
-	}*/
 	
-}     
+     
 
 $(document).ready(function(){
 	$('#ok-button').on('click', whenUserClicks);
 });
 
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
